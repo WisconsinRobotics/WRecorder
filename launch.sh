b0080ae2b9cffb9e6eaf93e1287d9353bc9f77ac
DefiantBurger <joseph.cicalese@gmail.com>
2025-10-12 14:49:11 -0500

    Changed launch script

--- launch.sh
+++ launch.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-cd /WRecorder
-source env/bin/activate
-python3 camera_streamer.py --base-port 5555 --camera-ids 0 2
-cd /
+/home/wiscrobo/WRecorder/env/bin/python3 /home/wiscrobo/WRecorder/camera_streamer.py --base-port 5555 --camera-ids 0 2
